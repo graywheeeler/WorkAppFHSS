88a52d67bd858ef6f2636082b04e55a1517077c4
graywheeeler <33742396+graywheeeler@users.noreply.github.com>
2025-05-15 13:11:13 -0600

    setup

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'FhssStarterApp' title`, () => {
+  it(`should have the 'TrainingApp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FhssStarterApp');
+    expect(app.title).toEqual('TrainingApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FhssStarterApp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, TrainingApp');
   });
 });
